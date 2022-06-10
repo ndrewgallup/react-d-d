453a82992ec41d52046f93867514ea8081195049
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-06-10 14:43:37 -0500

    set state in app.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
+import {useState} from 'react';
 
+import './App.css';
+
+function App(){
+  const [navItems, setNavItems] = useState([
+    {url: "/class-list", name: "Class List"}, 
+    {url: "/monster-list", name: "Scary Monsters"}, 
+    {url: "/spell-search", name: "Search for Spells"}
+  ])
+
+  return (
+    <>
+    <h1>APP!</h1>
+    </>
+  )
+}
+
+export default App;
